8d503bb67d5839502ad96c33111a98cd3bd594c7
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-08 06:28:04 +0500

    Create basic card component

---
+++ React_Learning/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card({ title, description, color }) {
+  return (
+    <div style={{ borderLeft: `4px solid ${color}` }}>
+      <h3>{title}</h3> <p>{description}</p>{" "}
+    </div>
+  );
+}
+
+export default Card;
