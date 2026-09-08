3a9ba190bc42236fbfa13be15bcc578fcc695581
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-08 06:42:01 +0500

    Create toggle component

---
+++ React_Learning/src/components/Toggle.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function Toggle() {
+    const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setIsVisible(!isVisible)}>
+        {isVisible ? 'Hide' : 'Show'}
+      </button>
+      {isVisible && <p>This is the content to toggle.</p>}
+    </div>
+  )
+}
