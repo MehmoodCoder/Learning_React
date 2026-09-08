85e275df83edfa2dada13c12c0c309529b34334f
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-08 06:31:10 +0500

    Remove extra code

--- React_Learning/src/App.jsx
+++ React_Learning/src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header/>
