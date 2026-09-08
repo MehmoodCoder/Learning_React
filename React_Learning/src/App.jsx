0718bb50bb42b969e28a897d3c122035baa8ca6e
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-08 06:39:29 +0500

    Import and use Counter

--- React_Learning/src/App.jsx
+++ React_Learning/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Card from './components/Card'
+import Counter from './components/Counter'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Card title="React" description="JS library for UIs" color="#A78BFA" />
       <Card title="Vite" description="Fast build tool" color="#00C864" />
       <Card title="Tailwind" description="Utility-first CSS" color="#00B4D8" />
+      <Counter/>
     </>
   )
 }
