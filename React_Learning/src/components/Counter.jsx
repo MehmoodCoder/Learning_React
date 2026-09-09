91be7dc59fbe3ccfc976c4d9cfe97b5e484ded4a
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-09 06:46:31 +0500

    Add useEffect

--- React_Learning/src/components/Counter.jsx
+++ React_Learning/src/components/Counter.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
 
   return (
     <div>
