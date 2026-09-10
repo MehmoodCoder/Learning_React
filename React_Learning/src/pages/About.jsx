7985e66934125207f515121a6cff3f757e010db0
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-10 06:28:31 +0500

    Create About page

---
+++ React_Learning/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div className="page-container">
+            <h1>About Me</h1>
+            <p>Hello! I am Mehmood Hassan, an aspiring developer building modern frontend UIs and full-stack web applications.</p>
+        </div>
+    );
+}
+
+export default About;
