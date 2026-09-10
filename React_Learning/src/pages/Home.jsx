f6fc833cea1cdbf21b09d6c87a371aa9d0214159
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-10 06:37:54 +0500

    Add features to Home page

--- React_Learning/src/pages/Home.jsx
+++ React_Learning/src/pages/Home.jsx
@@ -1,10 +1,28 @@
+import Card from "../components/Card";
+import Counter from "../components/Counter";
+import Toggle from "../components/Toggle";
+import QuoteBox from "../components/QuoteBox";
+import SkillsList from "../components/SkillsList";
+
 function Home() {
-    return (
-        <div className="page-container">
-            <h1>Home Page</h1>
-            <p>Welcome to my React Single Page Application (SPA). Explore pages seamlessly without full browser reloads.</p>
-        </div>
-    );
+  return (
+    <div className="home-dashboard">
+      <section className="tech-grid">
+        <Card title="React" description="JS library for UIs" color="#A78BFA" />
+        <Card title="Vite" description="Fast build tool" color="#00C864" />
+        <Card
+          title="Tailwind"
+          description="Utility-first CSS"
+          color="#00B4D8"
+        />
+      </section>
+
+      <Counter />
+      <Toggle />
+      <QuoteBox />
+      <SkillsList />
+    </div>
+  );
 }
 
 export default Home;
