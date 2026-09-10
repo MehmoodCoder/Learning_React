e94e4dfb1839084cfed54d07bbb07ea43965f42a
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-10 06:26:37 +0500

    Create Home page

---
+++ React_Learning/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="page-container">
+            <h1>Home Page</h1>
+            <p>Welcome to my React Single Page Application (SPA). Explore pages seamlessly without full browser reloads.</p>
+        </div>
+    );
+}
+
+export default Home;
