c4881016be0f9d3f067bf211d60eea579ae79e9b
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-10 06:39:12 +0500

    Create all pages routes

--- React_Learning/src/App.jsx
+++ React_Learning/src/App.jsx
@@ -1,24 +1,27 @@
-import './App.css'
-import Header from './components/Header'
-import Card from './components/Card'
-import Counter from './components/Counter'
-import Toggle from './components/Toggle'
-import QuoteBox from './components/QuoteBox'
-import SkillsList from './components/SkillsList'
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Card title="React" description="JS library for UIs" color="#A78BFA" />
-      <Card title="Vite" description="Fast build tool" color="#00C864" />
-      <Card title="Tailwind" description="Utility-first CSS" color="#00B4D8" />
-      <Counter/>
-      <Toggle/>
-      <QuoteBox/>
-      <SkillsList/>
-    </>
-  )
+    <div className="app-container">
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
