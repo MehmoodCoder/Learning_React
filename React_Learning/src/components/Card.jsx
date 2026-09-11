a9a1439f2bb005fe3655493402183fffd38bd1d5
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-11 06:36:24 +0500

    Update UI

--- React_Learning/src/components/Card.jsx
+++ React_Learning/src/components/Card.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-
 function Card({ title, description, color }) {
   return (
-    <div style={{ borderLeft: `4px solid ${color}` }}>
-      <h3>{title}</h3> <p>{description}</p>
+    <div className="bg-[#151c2c] rounded-xl p-6 border border-[#1e293b] hover:border-[#00C864] transition-all duration-300 shadow-md flex flex-col justify-between min-h-[140px]">
+      <div className="m-6">
+        <h3
+          className="text-xl font-bold m-6"
+          style={{ color: color || "#00C864" }}
+        >
+          &ensp;{title}
+        </h3>
+        <p className="text-slate-400 text-sm leading-relaxed">&ensp; &nbsp;{description}</p>
+      </div>
     </div>
   );
 }
